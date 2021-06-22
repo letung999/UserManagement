6d61c3725667b6f4515e43b0469ee1a116c1a5a0
LeTung <66472197+letung999@users.noreply.github.com>
2021-06-22 23:17:36 +0700

    Update Function.java

--- src/function/Function.java
+++ src/function/Function.java
@@ -6,18 +6,6 @@ import model.Teacher;
 import java.util.ArrayList;
 
 public interface Function {
-    /*
-    * a) Thêm mới giảng viên vào danh sách.
-b) Thêm mới sinh viên vào danh sách.
-c) Sắp xếp danh sách giảng viên theo tên giảm dần từ z-a.
-d) Sắp xếp danh sách giảng viên theo theo mức lương giảm dần.
-e) Sắp xếp danh sách giảng viên theo số năm kinh nghiệm giảm dần.
-f) Sắp xếp danh sách sinh viên theo tên a-z.
-g) Sắp xếp danh sách sinh viên theo điểm trung bình giảm dần.
-h) Hiển thị danh sách giảng viên theo dạng bảng gồm các hàng, cột.
-i) Hiển thị danh sách sinh viên theo dạng bảng gồm các hàng, cột.
-j) Thoát chương trình.*/
-
     /**
      * @param list
      * @param fileName
